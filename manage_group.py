5c52e544c9b597619e54d28bb4ee2c2ea1fc0470
jordanshaheen <jordan.shaheen@icloud.com>
2023-11-25 21:08:42 -0500

    only created function for part2, no implementation yet.

--- manage_group.py
+++ manage_group.py
@@ -1,19 +1,19 @@
 class GroupManager:
     def __init__(self):
-        self.groups = {}  # Store group information
+        self.groups = {}  # format: {group_name: set_of_usernames}
 
     def create_group(self, group_name):
-        # Code to create a new group
-        pass
+        if group_name not in self.groups:
+            self.groups[group_name] = set()
 
     def delete_group(self, group_name):
-        # Code to delete a group
-        pass
+        if group_name in self.groups:
+            del self.groups[group_name]
 
     def add_user_to_group(self, username, group_name):
-        # Code to add a user to a group
-        pass
+        if group_name in self.groups:
+            self.groups[group_name].add(username)
 
     def remove_user_from_group(self, username, group_name):
-        # Code to remove a user from a group
-        pass
+        if group_name in self.groups and username in self.groups[group_name]:
+            self.groups[group_name].remove(username)
